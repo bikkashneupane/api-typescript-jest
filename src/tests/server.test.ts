077f53a123d2c8caad487e36a37ef00f1860b77a
bikashneupane17 <edubikkash@gmail.com>
2024-12-18 22:54:09 +1100

    github actions ci.yml file added

--- src/tests/server.test.ts
+++ src/tests/server.test.ts
@@ -5,7 +5,8 @@ import request from "supertest";
 describe("Test server connection", () => {
   it("Should send 200 status message", async () => {
     const res = await request(app).get("/");
+    console.log(res);
     expect(res.status).toBe(200);
-    expect(res.text).toBe("Server alive");
+    expect(res.text).toBe("message: {Server alive}");
   });
 });
